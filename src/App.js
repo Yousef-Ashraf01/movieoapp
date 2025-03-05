177f1930f7a64138184e78adf2486335e792c2ab
yousef <yusifashraf501@gmail.com>
2025-03-05 04:57:23 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MobileNavigation from "./components/MobileNavigation";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setBannerData, setImageUrl } from "./store/movieoSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const fetchTrendingData = async () => {
+    try {
+      const response = await axios.get(
+        "/trending/all/week?api_key=b411f5c3625698399966c05ad323ccb5"
+      );
+
+      dispatch(setBannerData(response.data.results));
+
+      console.log("response", response.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const fetchConfiguration = async () => {
+    try {
+      const response = await axios.get(
+        "/configuration?api_key=b411f5c3625698399966c05ad323ccb5"
+      );
+
+      dispatch(setImageUrl(response.data.images.secure_base_url + "original"));
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchTrendingData();
+    fetchConfiguration();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="pb-14 lg:pb-0">
+      <Header />
+      <div className="min-h-[90vh]">
+        <Outlet />
+      </div>
+      <Footer />
+      <MobileNavigation />
+    </main>
   );
 }
 
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const useFetch = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        endpoint + "?api_key=b411f5c3625698399966c05ad323ccb5"
+      );
+      setLoading(false);
+      setData(response.data.results);
+    } catch (error) {
+      console.log("error ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [endpoint]);
+
+  return { data, loading };
+};
